86fe0b90cfc4fa5c2778315520e9632d49b3268f
Lars Willuweit <larswilluweit@mac.localdomain>
2025-05-31 16:35:43 +0200

    add operation implemented

--- backend/backend_app.py
+++ backend/backend_app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -8,6 +8,30 @@ POSTS = [
     {"id": 1, "title": "First post", "content": "This is the first post."},
     {"id": 2, "title": "Second post", "content": "This is the second post."},
 ]
+
+
+def validate_blog_post(dict_blog_post):
+    if 'title' not in dict_blog_post or 'content' not in dict_blog_post:
+        return False
+    return True
+
+
+@app.route('/api/posts', methods = ['POST'])
+def add_posts():
+    new_blog_post = request.get_json()
+    print(new_blog_post)
+    if not validate_blog_post(new_blog_post):
+        return 'Input invalid', 400
+
+    if len(POSTS) > 0:
+        new_id = max(blog_post['id'] for blog_post in POSTS) + 1
+    else:
+        new_id = 1
+
+    new_blog_post['id'] = new_id
+
+    POSTS.append(new_blog_post)
+    return jsonify(new_blog_post), 201
 
 
 @app.route('/api/posts', methods=['GET'])
